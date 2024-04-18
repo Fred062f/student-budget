241a524347b4b5ef9592fd3a4679ca37b9af534d
Frederik <frederik.damsgaard01@gmail.com>
2024-04-18 09:38:46 +0200

    Create Navigation

---
+++ app/src/main/java/com/example/student_budget/Navigation.kt
@@ -0,0 +1,14 @@
+package com.example.student_budget
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "screen1") {
+        composable("screen1") { HomeScreen(navController) }
+        composable("screen2") { NumbersScreen(navController) }
+    }
+}
