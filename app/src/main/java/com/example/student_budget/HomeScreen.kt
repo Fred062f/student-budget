bfec91487a3aa119fdad8b6fbc4b386b20951a93
Frederik <frederik.damsgaard01@gmail.com>
2024-04-18 09:55:49 +0200

    Imports

--- app/src/main/java/com/example/student_budget/HomeScreen.kt
+++ app/src/main/java/com/example/student_budget/HomeScreen.kt
@@ -1,17 +1,16 @@
 package com.example.student_budget
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import java.lang.reflect.Modifier
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
