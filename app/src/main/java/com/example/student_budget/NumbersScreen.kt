c89a950138823b1b2cbe58b9829dae9f1a1326b4
andreascmbuch <anbu0001@stud.kea.dk>
2024-04-18 09:59:55 +0200

    numberscreen

--- app/src/main/java/com/example/student_budget/NumbersScreen.kt
+++ app/src/main/java/com/example/student_budget/NumbersScreen.kt
@@ -1,2 +1,14 @@
 package com.example.student_budget
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun NumbersScreen(navController: NavController) {
+    Column () {
+
+    }
+    
+}
