f80649e16bc29a29b0e9f666da1556ae39a268b8
andreascmbuch <anbu0001@stud.kea.dk>
2024-04-18 11:25:14 +0200

    numberscreen

--- app/src/main/java/com/example/student_budget/NumbersScreen.kt
+++ app/src/main/java/com/example/student_budget/NumbersScreen.kt
@@ -1,14 +1,46 @@
 package com.example.student_budget
 
+
 import androidx.compose.foundation.layout.Column
+
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
-fun NumbersScreen(navController: NavController) {
-    Column () {
+fun NumbersScreen() {
 
+    var incomeValue by remember { mutableStateOf(TextFieldValue("0")) }
+    var expenseValue by remember { mutableStateOf(TextFieldValue("0")) }
+
+    val income = incomeValue.text.toIntOrNull() ?: 0
+    val expenses = expenseValue.text.toIntOrNull() ?: 0
+
+    val allowanceValue = income - expenses
+
+    Column {
+
+        TextField(
+            value = incomeValue,
+            onValueChange = {
+                incomeValue = it
+            },
+            label = { Text("Income") }
+        )
+
+        TextField(
+            value = expenseValue,
+            onValueChange = {
+                expenseValue = it
+            },
+            label = { Text("Fixed Expenses") }
+        )
+
+        Text("Allowance: $allowanceValue")
     }
-    
 }
+
+
+
+
